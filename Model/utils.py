5dfabfe4c643fc0541d06a39fc9ee6b5ff055045
yasser <yassermohamed166@icloud.com>
2025-04-28 18:27:19 +0300

    update to include face tags

--- Model/utils.py
+++ Model/utils.py
@@ -169,3 +169,49 @@ def parse_knowledge_to_string(knowledge: dict) -> str:
     knowledge: {knowledge['knowledge']}
     memory_ids: {knowledge['memory_ids']}\n\n'''
     return knowledge_string
+
+####################################################################################
+####################################################################################
+
+def parse_memory_to_string_update(memory: dict) -> str:
+    filename = memory['filename']
+
+    capture_method = memory['metadata']['capture_method']
+
+    temporal = memory['metadata']['temporal_info']
+    date_string = temporal['date_string']
+    day_of_week = temporal['day_of_week']
+    time_of_the_day = temporal['time_of_the_day']
+    temporal_info = f'{date_string}, {day_of_week}, {time_of_the_day}'
+
+    location = memory['metadata']['location']
+    address = location.get('address', 'Unknown')
+
+    content = memory['content']
+
+    caption = content.get('caption', '')
+    objects = content.get('objects', [])
+    people = content.get('people', [])
+    activities = content.get('activities', [])
+    text = content.get('text', '')
+    speech = content.get('speech', '')
+    face_tags = content.get('face_tags', [])
+
+    word_count = count_words(text)
+    text_in_prompt = text if word_count < 100 else ""
+
+    memory_string = f'''
+memory_id: {filename}
+capture method: {capture_method}
+temporal info: {temporal_info}
+location: {address}
+
+Content: 
+caption: {caption}
+visible objects: {objects}
+visible people: {people}
+face tags: {face_tags}
+visible text: {text_in_prompt}
+heard speech: {speech}
+inferred activities: {activities}\n\n'''
+    return memory_string
