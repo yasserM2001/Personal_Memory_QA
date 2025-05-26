3a1799a39c212abae1c54cb6d8f355f38be445a0
yasser <yassermohamed166@icloud.com>
2025-05-26 17:37:45 +0300

    Add ProtectedRoute component for route protection

--- Frontend/src/pages/index.jsx
+++ Frontend/src/pages/index.jsx
@@ -1,23 +1,38 @@
-import { Route, Routes } from 'react-router-dom'
-// pages
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home';
 import Register from '../components/auth/Register';
 import Initialize from './Initialize';
 import Login from '../components/auth/Login';
 import About from './About';
+import ProtectedRoute from '../components/auth/ProtectedRoute';
 
 export default function Pages() {
   return (
-    <div className="p-4">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/init' element={<Initialize />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About />} />
-
-        
-      </Routes>
+    <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route 
+            path='/init' 
+            element={
+              <ProtectedRoute>
+                <Initialize />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path='/about' 
+            element={
+              <ProtectedRoute>
+                <About />
+              </ProtectedRoute>
+            } 
+          />
+          {/* Redirect any unknown routes to home */}
+          <Route path='*' element={<Navigate to="/" replace />} />
+        </Routes>
     </div>
-  )
+  );
 }
